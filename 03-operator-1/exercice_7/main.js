c0e5d24ab2b1fc7fe47f12e7e4cb85f3fd38c615
keren2906 <kerenmatuli2906@gamil.com>
2025-10-10 18:43:56 +0200

    feat: Create a variable  with a value, then set  to twice the child's age

--- 03-operator-1/exercice_7/main.js
+++ 03-operator-1/exercice_7/main.js
@@ -1,2 +1,13 @@
 // Exercice 7
 // Écrivez votre code ici
+let ageEnfant = 10;
+let agePere = ageEnfant * 2;
+let ageMere = agePere - 5;
+let ageGrandPere = (ageMere * 2) + (ageEnfant / 2);
+let ageOncle = agePere + 10;
+
+console.log("Âge de l’enfant :", ageEnfant);
+console.log("Âge du père :", agePere);
+console.log("Âge de la mère :", ageMere);
+console.log("Âge du grand-père :", ageGrandPere);
+console.log("Âge de l’oncle :", ageOncle);
