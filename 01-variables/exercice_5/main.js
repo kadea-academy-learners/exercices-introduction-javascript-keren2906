efa530fd49f06206d47533699550b923eabde990
keren2906 <kerenmatuli2906@gamil.com>
2025-10-10 18:13:05 +0200

    feat: Write a small script that uses let to store a temperature in Celsius, converts it to Fahrenheit, and displays the result

--- 01-variables/exercice_5/main.js
+++ 01-variables/exercice_5/main.js
@@ -1,2 +1,11 @@
 // Exercice 5
 // Écrivez votre code ici
+function celsiusToFahrenheit(celsius) {
+  return (celsius * 9/5) + 32;
+}
+
+let celsius = 25;
+let fahrenheit = celsiusToFahrenheit(celsius);
+console.log(celsius + "°C équivaut à " + celsiusToFahrenheit(celsius) + "°F");
+
+module.exports = {celsiusToFahrenheit};
