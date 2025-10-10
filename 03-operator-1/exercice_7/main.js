bab8dbb2d7c7b5d90ebca8f85e1c4297f4ddd1b0
keren2906 <kerenmatuli2906@gamil.com>
2025-10-10 18:53:13 +0200

    fix:adding the module

--- 03-operator-1/exercice_7/main.js
+++ 03-operator-1/exercice_7/main.js
@@ -11,3 +11,7 @@ console.log("Âge du père :", agePere);
 console.log("Âge de la mère :", ageMere);
 console.log("Âge du grand-père :", ageGrandPere);
 console.log("Âge de l’oncle :", ageOncle);
+
+module.exports= {
+    ageEnfant, ageGrandPere, ageMere, ageOncle, agePere
+}
