f9dbab94224be4ecf6b543c8d0f80e5c64648209
keren2906 <kerenmatuli2906@gamil.com>
2025-10-10 19:02:10 +0200

    feat: adding the exercise solution

--- 03-operator-1/exercice_8/main.js
+++ 03-operator-1/exercice_8/main.js
@@ -1,2 +1,17 @@
 // Exercice 8
 // Écrivez votre code ici
+let salaireMensuel = 500;
+let loyer = salaireMensuel * 0.3;
+let nourriture = salaireMensuel * 0.2;
+let transport = salaireMensuel * 0.1;
+let autresDepenses = 50;
+autresDepenses += 25;
+let totalDepenses = loyer + nourriture + transport + autresDepenses;
+let reste = salaireMensuel - totalDepenses;
+console.log("Salaire mensuel :", salaireMensuel);
+console.log("Loyer :", loyer);
+console.log("Nourriture :", nourriture);
+console.log("Transport :", transport);
+console.log("Autres dépenses :", autresDepenses);
+console.log("Total des dépenses :", totalDepenses);
+console.log("Reste :", reste);
