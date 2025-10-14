adc43ff58dfc169d78f98de25da5b3d2a3f1ce2d
keren2906 <kerenmatuli2906@gamil.com>
2025-10-14 06:11:48 +0200

    fix: modification or correction of exercice 9

--- 03-operator-1/exercice_9/main.js
+++ 03-operator-1/exercice_9/main.js
@@ -3,43 +3,34 @@
 let maison = 60000000;
 let terrains = 40000000;
 let liquidites = 20000000;
-
-let patrimoineTotal = maison + terrains + liquidites;
-
-let partEnfants = patrimoineTotal * 0.75;
-
-let partAutres = patrimoineTotal * 0.25;
-
-let partParEnfant = partEnfants / 3;
-
+let totalHeritage = 6000000 + 4000000 + 2000000;
+let heritageEnfants = totalHeritage * 0.75;
+let partParEnfant = heritageEnfants/ 3;
+let paul = partParEnfant;
 let marie = partParEnfant;
+let eric = parAlain / 2;
+let claire = parAlain/ 2;
 
-let eric = partAlain / 2;
-let claire = partAlain / 2;
-
-
+let heritageDeuxiemeCategorie = totalHeritage * 0.25;
 let partParPersonneCat2 = partAutres / 3;
 
 let madameMukuna = partParPersonneCat2;
 let joseph = partParPersonneCat2;
-
 let sarah = partParPersonneCat2;
 
-console.log("=== RÉPARTITION DE L’HÉRITAGE DE M. JEAN MUKUNA ===");
-console.log(`Patrimoine total : ${patrimoineTotal.toLocaleString()} CDF\n`);
-
+console.log("RÉPARTITION DE L’HÉRITAGE DE M. JEAN MUKUNA");
 console.log(">> Première catégorie (75%) : Enfants");
-console.log(`Paul : ${paul.toLocaleString()} CDF`);
-console.log(`Marie : ${marie.toLocaleString()} CDF`);
-console.log(`Éric : ${eric.toLocaleString()} CDF`);
-console.log(`Claire : ${claire.toLocaleString()} CDF\n`);
+console.log("Paul :", paul, "CDF");
+console.log("Marie :", marie,  "CDF");
+console.log("Éric :", e, "CDF");
+console.log("Claire :",claire,"CDF");
 
 console.log(">> Deuxième catégorie (25%) : Conjoint et frères");
-console.log(`Madame MUKUNA : ${madameMukuna.toLocaleString()} CDF`);
-console.log(`Joseph : ${joseph.toLocaleString()} CDF`);
-console.log(`Sarah : ${sarah.toLocaleString()} CDF`);
+console.log("Madame MUKUNA :",madameMukuna, "CDF");
+console.log("Joseph :",joseph, "CDF");
+console.log("Sarah :", sarah, "CDF");
 
 
 module.exports= {
-    sarah, joseph, madameMukuna, claire, eric, marie, paul, patrimoineTotal
-}
+    paul, marie, eric, claire, madameMukuna, joseph, sarah 
+};
